ca4ad84ba85f3cefcba549ae808edaea37343095
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2013-07-04 19:33:49 +0200

    Adding code to extract images from a PDF file

---
+++ src/pdfcreator/ImageRenderListener.java
@@ -0,0 +1,69 @@
+/*
+ *  Copyright (c) 2011 . Patrick Hochstenbach <Patrick.Hochstenbach@gmail.com>
+ */
+package pdfcreator;
+
+import com.itextpdf.text.pdf.PdfName;
+import com.itextpdf.text.pdf.parser.ImageRenderInfo;
+import com.itextpdf.text.pdf.parser.PdfImageObject;
+import com.itextpdf.text.pdf.parser.RenderListener;
+import com.itextpdf.text.pdf.parser.TextRenderInfo;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ *
+ * @author hochsten
+ */
+public class ImageRenderListener implements RenderListener {
+    /** The new document to which we've added a border rectangle. */
+    protected String path = "";
+    protected int page = 0;
+ 
+    /**
+     * Creates a RenderListener that will look for images.
+     */
+    public ImageRenderListener(String path) {
+        this.path = path;
+    }
+ 
+    /**
+     * @see com.itextpdf.text.pdf.parser.RenderListener#beginTextBlock()
+     */
+    public void beginTextBlock() {
+    }
+ 
+    /**
+     * @see com.itextpdf.text.pdf.parser.RenderListener#endTextBlock()
+     */
+    public void endTextBlock() {
+    }
+ 
+    /**
+     * @see com.itextpdf.text.pdf.parser.RenderListener#renderImage(
+     *     com.itextpdf.text.pdf.parser.ImageRenderInfo)
+     */
+    public void renderImage(ImageRenderInfo renderInfo) {
+        try {
+            String filename;
+            FileOutputStream os;
+            PdfImageObject image = renderInfo.getImage();
+            if (image == null) return;
+            filename = String.format("%s/%s.%s",path, ++page, image.getFileType());
+            os = new FileOutputStream(filename);
+            System.out.println("creating file " + filename);
+            os.write(image.getImageAsBytes());
+            os.flush();
+            os.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+ 
+    /**
+     * @see com.itextpdf.text.pdf.parser.RenderListener#renderText(
+     *     com.itextpdf.text.pdf.parser.TextRenderInfo)
+     */
+    public void renderText(TextRenderInfo renderInfo) {
+    }
+}
---
+++ src/pdfcreator/PDFImageExtract.java
@@ -0,0 +1,48 @@
+/*
+ *  Copyright (c) 2011 . Patrick Hochstenbach <Patrick.Hochstenbach@gmail.com>
+ */
+package pdfcreator;
+
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.parser.PdfReaderContentParser;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author hochsten
+ */
+public class PDFImageExtract {
+    public PDFImageExtract() {}
+    
+    public void extract(String filename, String path) throws IOException {
+        PdfReader reader = new PdfReader(filename);
+        ImageRenderListener listener = new ImageRenderListener(path);
+        
+        PdfReaderContentParser parser = new PdfReaderContentParser(reader);
+        
+        for (int i = 1; i <= reader.getNumberOfPages(); i++) {
+            parser.processContent(i, listener);
+        }
+        
+    }
+    
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("usage: PDFImageExtract file out");
+            System.exit(1);
+        }
+        
+        String filename = args[0];
+        String output = args[1];
+        
+        if (! (new File(output)).isDirectory()) {
+            System.err.println("output " + output + " isn't a directory");
+        }
+        
+        PDFImageExtract tool = new PDFImageExtract();
+        
+        tool.extract(filename,output);
+    }
+}
